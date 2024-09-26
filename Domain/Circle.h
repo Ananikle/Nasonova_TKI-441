4f445a592b3e5e731740b622241bfdd21c093143
Анастасия Насонова <1146235@edu.rut-miit.ru>
2024-09-26 12:05:03 +0300

    Создание пространства имен namespace "circle"

--- Domain/Circle.h
+++ Domain/Circle.h
@@ -1,34 +1,38 @@
 #pragma once
 
-/**
-* @brief Класс Круг
-*/
-class Circle
+namespace circle
 {
-private:
 	/**
-	* @brief Радиус
+	* @brief Класс Круг
 	*/
-	double radius;
-public:
-	/*
-	* @brief Вернуть радиус
-	* @return Радиус
-	*/
-	double GetRadius() const;
-	/*
-	* @brief Рассчитать длину окружности
-	* @return Длина окружности
-	*/
-	double GetLength() const;
-	/*
-	* @brief Рассчитать площадь окружности
-	* @return Площадь
-	*/
-	double GetArea() const;
-	/*
-	* @brief Инициализирует новый объект класса Circle
-	* @param radius радиус
-	*/
-	Circle(const double radius);
-};
+	class Circle
+	{
+	private:
+		/**
+		* @brief Радиус
+		*/
+		double radius;
+	public:
+		/*
+		* @brief Вернуть радиус
+		* @return Радиус
+		*/
+		double GetRadius() const;
+		/*
+		* @brief Рассчитать длину окружности
+		* @return Длина окружности
+		*/
+		double GetLength() const;
+		/*
+		* @brief Рассчитать площадь окружности
+		* @return Площадь
+		*/
+		double GetArea() const;
+		/*
+		* @brief Инициализирует новый объект класса Circle
+		* @param radius радиус
+		*/
+		Circle(const double radius);
+	};
+}
+
