5dbc273cb0758010d094ed7eac75dbb63fcface9
Анастасия Насонова <1146235@edu.rut-miit.ru>
2024-09-26 11:49:27 +0300

    Реализация заголовка класса "Круг"

--- Domain/Circle.h
+++ Domain/Circle.h
@@ -1,1 +1,34 @@
 #pragma once
+
+/**
+* @brief Класс Круг
+*/
+class Circle
+{
+private:
+	/**
+	* @brief Радиус
+	*/
+	double radius;
+public:
+	/*
+	* @brief Вернуть радиус
+	* @return Радиус
+	*/
+	double GetRadius() const;
+	/*
+	* @brief Рассчитать длину окружности
+	* @return Длина окружности
+	*/
+	double GetLength() const;
+	/*
+	* @brief Рассчитать площадь окружности
+	* @return Площадь
+	*/
+	double GetArea() const;
+	/*
+	* @brief Инициализирует новый объект класса Circle
+	* @param radius радиус
+	*/
+	Circle(const double radius);
+};
