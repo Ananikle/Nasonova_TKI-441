ec9ebc54551d1f642ac448a003f09039fb5789af
Анастасия Насонова <1146235@edu.rut-miit.ru>
2024-10-27 14:59:11 +0300

    Добавление тестов

--- UnitTest/UnitTest.cpp
+++ UnitTest/UnitTest.cpp
@@ -1,16 +1,97 @@
 #include "pch.h"
 #include "CppUnitTest.h"
 
+#include "../Domain/Circle.h"
+#include "../Domain/Circle.cpp"
+
+#include <string>
+#include <sstream>
+#include <iomanip>
+
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 namespace UnitTest
 {
-	TEST_CLASS(UnitTest)
+	TEST_CLASS(CircleTests)
 	{
 	public:
-		
-		TEST_METHOD(TestMethod1)
+
+		TEST_METHOD(Constructor_NegativeRadius_ExpectedException)
 		{
+			// Assert 
+			Assert::ExpectException<std::invalid_argument>([]() { circle::Circle obj(-2.8); });
+		}
+
+		TEST_METHOD(GetRadius_ValidData_Success)
+		{
+			// Arrange
+			const double R = 2.8;
+			circle::Circle obj{ R };
+			const double expected = R;
+			const double epsilon = 0.001;
+			// Act
+			const double actual = obj.GetRadius();
+			// Assert 
+			Assert::IsTrue(std::abs(expected - actual) <= epsilon);
+		}
+
+		TEST_METHOD(GetDiameter_ValidData_Success)
+		{
+			// Arrange
+			const double R = 2.8;
+			circle::Circle obj{ R };
+			const double expected = R * 2;
+			const double epsilon = 0.001;
+			// Act
+			const double actual = obj.GetDiameter();
+			// Assert 
+			Assert::IsTrue(std::abs(expected - actual) <= epsilon);
+		}
+
+		TEST_METHOD(GetLength_ValidData_Success)
+		{
+			// Arrange
+			const double R = 2.8;
+			circle::Circle obj{ R };
+			const double expected = 2 * 3.14 * R;
+			const double epsilon = 0.01;
+			// Act
+			const double actual = obj.GetLength();
+			// Assert 
+			Assert::IsTrue(std::abs(expected - actual) <= epsilon);
+		}
+
+		TEST_METHOD(GetArea_ValidData_Success)
+		{
+			// Arrange
+			const double R = 2.8;
+			circle::Circle obj{ R };
+			const double expected = 3.14 * R * R;
+			const double epsilon = 0.1;
+			// Act
+			const double actual = obj.GetArea();
+			// Assert 
+			Assert::IsTrue(std::abs(expected - actual) <= epsilon);
+		}
+
+		TEST_METHOD(OperatorLeftShift_ValidData_Success)
+		{
+			// Arrange
+			const double R = 2.8;
+			circle::Circle obj{ R };
+			const std::string expected =
+				"Радиус окружности: " + std::to_string(obj.GetRadius()) + "\n" +
+				"Диаметр окружности: " + std::to_string(obj.GetDiameter()) + "\n" +
+				"Длина окружности: " + std::to_string(obj.GetLength()) + "\n" +
+				"Площадь окружности: " + std::to_string(obj.GetArea()) + "\n";
+			std::stringstream buffer{};
+
+			// Act
+			buffer << std::fixed << obj;
+			std::string actual = buffer.str();
+
+			// Assert
+			Assert::AreEqual(expected, actual);
 		}
 	};
 }
